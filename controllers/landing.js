ec6c3d1d0ea050f71f9ad543dc3787976ae012d9
cfierke <46982767+cfierke@users.noreply.github.com>
2020-02-17 15:58:45 -0600

    New routes to leads

--- controllers/landing.js
+++ controllers/landing.js
@@ -1,10 +1,19 @@
-
+const models = require('../models');
 
 exports.get_landing = function(req, res, next) {
   res.render('landing', { title: 'Express' });
 }
 
 exports.submit_lead = (req, res, next) => {
-  console.log("lead email: ", req.body.lead_email);
-  res.redirect('/');
+  return models.Lead.create({
+    email: req.body.lead_email
+  }).then(lead => {
+    res.redirect('/leads');
+  })
+}
+
+exports.show_leads = (req, res, next) => {
+  models.Lead.findAll().then(leads => {
+    res.render('landing', { title: 'Express', leads: leads })
+  })
 }
